56811127417242d7f5d5dbd3c05dea6c97a6398b
sebasvaldez <marcelosebastianvaldez@gmail.com>
2023-07-08 00:07:27 -0300

    Corrigiendo css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,20 +4,23 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="page-footer font-small blue pt-4">
-      <div className="container-fluid text-center text-md-left">
+      <div className="container-fluid text-center">
         <div className="row">
-          <div className="col-md-6 mt-md-0 mt-3">
-            <h5 className="text-uppercase link mb-2">Proyecto final - curso React</h5>
-            
+          <div className=" mt-md-0 mt-3">
+            <h5 className="text-uppercase link mb-2 text-center">
+              Proyecto final - curso React
+            </h5>
           </div>
 
           <hr className="clearfix w-100 d-md-none py-0 mt-2 " />
-
         </div>
       </div>
 
-      <div className="footer-copyright text-center py-3">
-        <Link to="https://github.com/sebasvaldez" className="link"> Marcelo Sebastián Valdez</Link>
+      <div className="text-center py-3">
+        <Link to="https://github.com/sebasvaldez" className="link">
+          {" "}
+          Marcelo Sebastián Valdez
+        </Link>
       </div>
     </footer>
   );
